a642d1a168105f336a62f053e719b48658d37e84
Milos Danilov <milosdanilov@gmail.com>
2024-02-20 23:34:07 +0100

    90. working with ejs

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const handlebars = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', handlebars({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-}));
-
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
