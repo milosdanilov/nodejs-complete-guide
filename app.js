1d5f9fd89f049bfc4bdd31b45bc66bd720b3ed49
Milos Danilov <milosdanilov@gmail.com>
2024-03-25 22:37:36 +0100

    99. Finishing the controller

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -13,11 +15,8 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
