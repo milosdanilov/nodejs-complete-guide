685bdbaef2ee199de1aa6850e7740bdf4408d7a3
Milos Danilov <milosdanilov@gmail.com>
2024-12-04 11:13:03 +0100

    111. storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,10 +3,7 @@ const Product = require('../models/products');
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', { 
         pageTitle: 'Add product', 
-        path: '/admin/add-product', 
-        formsCSS: true, 
-        productCSS: true, 
-        activeAddProduct: true,
+        path: '/admin/add-product',
     });
 };
 
@@ -21,7 +18,12 @@ exports.getAdminProducts = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(title, imageUrl, description, price);
     product.save();
 
     res.redirect('/');
