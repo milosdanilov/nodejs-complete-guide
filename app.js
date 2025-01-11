44f76544dedf3385652b85222be3acc5229b3178
Milos Danilov <milosdanilov@gmail.com>
2025-01-11 21:53:26 +0100

    Section 9: Dynamic routes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
@@ -12,11 +10,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use(errorController.get404);
 
 app.listen(3000);
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,30 +1,31 @@
-const Product = require('../models/products');
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { 
-        pageTitle: 'Add product', 
-        path: '/admin/add-product',
-    });
-};
-
-exports.getAdminProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', { 
-            prods: products, 
-            pageTitle: 'Admin products', 
-            path: '/admin/products',
-        });
-    });
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
 
-    const product = new Product(title, imageUrl, description, price);
-    product.save();
-
-    res.redirect('/');
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
 };
