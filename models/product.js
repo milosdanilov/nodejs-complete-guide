ef5fb0d7817d69af3e301e7ee13ccda0129b85ce
Milos Danilov <milosdanilov@gmail.com>
2025-01-11 22:17:02 +0100

    121. loading product details data

--- models/product.js
+++ models/product.js
@@ -38,4 +38,11 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
+  static findById(productId, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === productId);
+      cb(product);
+    });
+  }
 };
