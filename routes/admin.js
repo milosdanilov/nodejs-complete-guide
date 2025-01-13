0a1a57622244c07ddcf376ae878f3979f22f22c2
Milos Danilov <milosdanilov@gmail.com>
2025-01-13 11:29:15 +0100

    128. editing the product data

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,10 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-// /admin/edit-product => GET
+// /admin/edit-product/:productId => GET
 router.get('/edit-product/:productId', adminController.getEditProduct);
+
+// /admin/edit-product => POST
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
